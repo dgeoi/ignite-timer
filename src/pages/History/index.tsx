8edd06575d2c974401ea9e05bb136ca0769a7fc8
dgeoi <diegofreitas_oliveira@proton.me>
2023-04-08 19:47:06 -0300

    [src/pages/History]: Structure and style

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,36 @@
+import { HistoryContainer, HistoryContent } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryContent>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>When</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deserunt totam, aliquam praesentium esse fuga commodi, iste ab
+                qui laudantium, laborum eveniet enim ullam. Exercitationem
+                dignissimos expedita nam dolor labore molestias.
+              </td>
+              <td>45 minutes</td>
+              <td>2 months ago</td>
+              <td>Finished</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryContent>
+    </HistoryContainer>
+  )
 }
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  & > h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryContent = styled.div`
+  flex: 1 1 auto;
+  overflow: auto;
+  margin-top: 2rem;
+
+  & > table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      padding: 1rem;
+
+      text-align: left;
+      font-size: 0.875rem;
+      line-height: 1.6;
+      color: ${(props) => props.theme['gray-100']};
+      background-color: ${(props) => props.theme['gray-600']};
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      padding: 1rem;
+
+      line-height: 1.6;
+      font-size: 0.875rem;
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+
+      &:first-child {
+        max-width: 440px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
